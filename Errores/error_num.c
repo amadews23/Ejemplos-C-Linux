27276265d27ff94caacb3c26439730728ac5eff9
Bartolomé Vich Lozano <tolo@tovilo.es>
2019-09-29 18:31:02 +0200

    Create error_num.c

---
+++ Errores/error_num.c
@@ -0,0 +1,34 @@
+/********************************************************
+* NOMBRE: #Bartolome#
+* PRIMER APELLIDO: #Vich#
+* SEGUNDO APELLIDO: #Lozano#
+* EMAIL: #amadews23@hotmail.com#
+********************************************************/
+/*******************************************************
+Programa error_num.c
+Descripcion:
+	Imprime lista de errores
+********************************************************/
+
+#include <stdio.h>
+
+//extern char *sys_errlist []; No compila
+extern __const char *__const sys_errlist[];
+extern int sys_nerr;
+
+
+//Al compilar muestra la salida:
+//aviso: `sys_errlist' is deprecated; use `strerror' or `strerror_r' instead
+//aviso: `sys_nerr' is deprecated; use `strerror' or `strerror_r' instead
+
+int main () {
+
+	int i;
+	
+	for (i=0; i < sys_nerr; i++) {
+
+		printf("%d: %s\n", i, sys_errlist[i] );
+
+  }
+
+}
